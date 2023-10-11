684353741f88c3a72dbe916a8cd758bae76f7d4e
aras06001 <143995475+aras06001@users.noreply.github.com>
2023-10-11 12:34:34 +0200

    Initial commit

---
+++ Assets/Movment.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public float speed = 1f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(Time.deltaTime * speed,0,0);
+        }
+
+        if(Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(Time.deltaTime * -speed,0,0);
+        }
+    }
+}
