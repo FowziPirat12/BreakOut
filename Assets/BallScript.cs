8da019e2460c9ac7fca242c5193430981349dbf2
vikkan <Jakobsenviktor6@gmail.com>
2023-11-08 12:10:28 +0100

    aswgsdetfg

--- Assets/BallScript.cs
+++ Assets/BallScript.cs
@@ -5,14 +5,34 @@ using UnityEngine;
 public class BallScript : MonoBehaviour
 {
     // Start is called before the first frame update
+
+    public float speed = 5f; 
+    public float minY = -6.9f; 
+    public float maxVelocity = 15f; 
+    Rigidbody2D rb;
+
+    
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector3(0,1,0);
+
+        rb.isKinematic = true;
+        rb.AddForce(new Vector2(0, speed));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (transform.position.y < minY)
+        {
+            transform.position = Vector3.zero; 
+            rb.velocity = Vector3.zero; 
+        }
+
+        if (rb.velocity.magnitude > maxVelocity)
+        {
+            rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxVelocity);
+        }
     }
 }
